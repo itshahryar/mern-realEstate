e0c3e13ed2dd29a9bb0d1114cf853da5ba4c76e5
itshahryar <shahryaramjadmos@gmail.com>
2024-08-07 04:55:22 -0700

    create and run the server

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import userRouter from './routes/user.route.js';
+// import authRouter from './routes/auth.route.js';
+// import listingRouter from './routes/listing.route.js';
+// import cookieParser from 'cookie-parser';
+// import path from 'path';
+
+const app = express();
+// app.use(express.json());
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+  });
+
