4e42c1d745d7f2373289e89bb14361d2ccb476b3
itshahryar <shahryaramjadmos@gmail.com>
2024-08-07 07:58:49 -0700

    sign up page ui

--- realEstate-frontend/src/pages/SignUp.jsx
+++ realEstate-frontend/src/pages/SignUp.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-6'>
+      <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          // onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          // onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          // onChange={handleChange}
+        />
+        <button
+          // disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {/* {loading ? 'Loading...' : 'Sign Up'} */}
+          Sign Up
+        </button>
+      </form>
+
+      <div className='flex gap-2 mt-4'>
+        <p>Already Have An Account ?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }
